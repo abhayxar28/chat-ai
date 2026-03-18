117cbc9fe1f2b9d1ad80e108d9ef835041db65bd
abhay rawat <abhayarrawat28@gmail.com>
2026-03-18 12:28:33 +0530

    Fix Express wildcard route

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -14,12 +14,12 @@ app.use(cors({
     credentials: true
 }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '../public/dist/index.html')));
+app.use(express.static(path.join(__dirname, '../public/dist')));
 
 app.use("/api/v1/users", authRouter)
 app.use("/api/v1/chats", chatRouter)
 
-app.get("/:path(*)", (req, res) => {
+app.get("/{*path}", (req, res) => {
   res.sendFile(path.join(__dirname, "public/dist/index.html"));
 });
 
